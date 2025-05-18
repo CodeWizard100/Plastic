07cf4e44d5f2ba90faf74273cdc77a8826a2df21
Codewizard <132087862+CodeWizard100@users.noreply.github.com>
2025-05-18 08:34:36 +0200

    Setup DirectX Includes and Libs

---
+++ Plastic_DirectX/linux.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "DirectX Dosent Work On Linux, try OpenGL or Vulkan" << std::endl;
+    return 0;
+}
--- Plastic_DirectX/src/main.cpp
+++ Plastic_DirectX/src/main.cpp
@@ -1,4 +1,11 @@
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
 #include <iostream>
+#include <d3d11.h>
+#include <directxmath.h>
+using namespace DirectX;
+
 int main() {
     std::cout << "Hello, Plastic!" << std::endl;
     return 0;
